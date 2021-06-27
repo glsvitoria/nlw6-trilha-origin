b6599a1ff0466ed26e004aa8218c40861fc35c3d
Guilherme Vitória <guivitoria2010@hotmail.com>
2021-06-27 01:58:21 -0300

    Adicionando as novidades da aula 4

--- scripts/main.js
+++ scripts/main.js
@@ -18,11 +18,10 @@ for(const link of links){
 }
 
 /* Mudar o header da página quando der scroll */
+function changeHeaderWhenScroll(){
+    const header = document.querySelector("#header")
+    const navHeight = header.offsetHeight
 
-const header = document.querySelector("#header")
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function() {
     if(window.scrollY >= navHeight){
         // Scroll é maior que a altura do header
         header.classList.add('scroll')
@@ -30,10 +29,8 @@ window.addEventListener('scroll', function() {
         // Menor que a altura do header
         header.classList.remove('scroll')
     }
-})
-
+}
 /* Testimonials swiper */
-
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     pagination: {
@@ -44,7 +41,6 @@ const swiper = new Swiper('.swiper-container', {
 });
 
 /* Mostrar elementos quando der scroll na página */
-
 const scrollReveal = ScrollReveal   ({
     origin: 'top',
     distance: '30px',
@@ -57,7 +53,25 @@ scrollReveal.reveal(
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
     `,
     { interval: 100 }
 )
+
+/* Botão voltar para o topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if(window.scrollY >= 560){
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* Função para chamar as duas funções anteriores de uma vez só */
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
