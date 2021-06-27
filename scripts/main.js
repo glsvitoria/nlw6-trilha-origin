19406fc7a1694899ac994db0d326e70b1083bfb4
Guilherme Vitória <guivitoria2010@hotmail.com>
2021-06-27 00:02:03 -0300

    Adicionando novidades da aula 3

--- scripts/main.js
+++ scripts/main.js
@@ -31,3 +31,33 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+/* Testimonials swiper */
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+});
+
+/* Mostrar elementos quando der scroll na página */
+
+const scrollReveal = ScrollReveal   ({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `,
+    { interval: 100 }
+)
